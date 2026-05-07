b67fb8e3b690d72bc3de960b3ee165698c49af4d
Shaanaya <pal.sayani.2005@gmail.com>
2026-05-07 12:31:21 +0530

    utils,exception & logger added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-End-Chest-cancer-classification-using-mlflow"
-AUTHOR_USER_NAME = "Sayani Pal"
+AUTHOR_USER_NAME = "sayani-pal"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "pal.sayani.2005@gmail.com"
 
